6332887e46c6f0f4395505e96813c10287084ba7
融雪时分 <cutedays@icloud.com>
2021-08-08 05:48:45 +0000

    Change CMakelists

--- src/core/kiraray.h
+++ src/core/kiraray.h
@@ -2,4 +2,10 @@
 
 #include <iostream>
 
+#include "assimp/Importer.hpp"
+#include "enoki/array.h"
+
 typedef float Float;
+
+typedef enoki::Array<Float, 3> Vec3f;
+typedef enoki::Matrix<Float, 3> Mat3f;
--- src/main/kiraray.cpp
+++ src/main/kiraray.cpp
@@ -1,6 +1,8 @@
 #include "kiraray.h"
 
 int main(int argc, char* argv[]){
+	Vec3f vec(2.0);
 	std::cout << "Hello World!" << std::endl;
+	std::cout << pow(vec, 3);
 	return 0;
 }
