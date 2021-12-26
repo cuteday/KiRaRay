37fc62895a7149af992ff9add02379ae199e2621
融雪时分 <cutedays@icloud.com>
2021-12-26 23:00:20 +0800

    updated for cuda integration

---
+++ src/cuda/common.h
@@ -0,0 +1,3 @@
+#include <cuda_runtime.h>
+
+__global__ void spgemm(const float *__restrict__ A, const float *__restrict__ B, float *__restrict__ C, int M, int N, int K);
---
+++ src/cuda/test.cu
@@ -0,0 +1,15 @@
+#include "common.h"
+#include <cuda_runtime.h>
+
+__global__ void spgemm(const float* __restrict__ A, const float* __restrict__ B, float* __restrict__ C,
+	int M, int N, int K){
+	int tx = blockIdx.x * blockDim.x + threadIdx.x;
+    int ty = blockIdx.y * blockDim.y + threadIdx.y;
+    if(ty < M && tx < N) {
+        float c = 0;
+        for(int i = 0; i < K; ++i){
+            c += A[ty * K + i] * B[i * N + tx];
+        }
+        C[ty * N + tx] = c;
+    }
+}
