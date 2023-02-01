66426dfdc73573a1a29185df71f0722b3eb0e594
融雪时分 <cutedays@icloud.com>
2023-02-01 23:30:09 +0800

    start to write an example with nv-rhi

---
+++ src/misc/samples/simple-rhi/main.cpp
@@ -0,0 +1,11 @@
+#include <common.h>
+#include <logger.h>
+#include <nvrhi/vulkan.h>
+
+KRR_NAMESPACE_BEGIN
+
+extern "C" int main(int argc, const char *argv[]) {
+	Log(Info, "Hello from simple-rhi!");
+}
+
+KRR_NAMESPACE_END
