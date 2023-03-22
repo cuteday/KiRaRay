1b9c90d123faff4f0068f092a6aad718f9e01453
融雪时分 <cutedays@icloud.com>
2023-03-22 12:50:11 +0800

    create initial files

---
+++ src/render/rasterize/bindless.h
@@ -0,0 +1,49 @@
+#pragma once
+#include "common.h"
+#include "renderpass.h"
+
+#include "vulkan/shader.h"
+#include "vulkan/binding.h"
+#include "vulkan/descriptor.h"
+#include "vulkan/helperpass.h"
+
+KRR_NAMESPACE_BEGIN
+
+class BindlessRender : public RenderPass {
+public:
+	struct ViewConstants {
+		Matrix4f worldToView;
+	};
+	
+	using RenderPass::RenderPass;
+	using SharedPtr = std::shared_ptr<BindlessRender>;
+	KRR_REGISTER_PASS_DEC(BindlessRender);
+
+	void initialize() override;
+	void render(RenderFrame::SharedPtr frame) override;
+	void renderUI() override;
+	void resize(const Vector2i &size) override;
+
+	string getName() const override { return "BindlessRender"; }
+
+private:
+	vkrhi::CommandListHandle mCommandList;
+	vkrhi::BindingLayoutHandle mBindingLayout;
+	vkrhi::BindingLayoutHandle mBindlessLayout;
+	vkrhi::BindingSetHandle mBindingSet;
+	vkrhi::ShaderHandle mVertexShader;
+	vkrhi::ShaderHandle mPixelShader;
+	vkrhi::GraphicsPipelineHandle mGraphicsPipeline;
+
+	vkrhi::BufferHandle mViewConstants;
+	vkrhi::TextureHandle mDepthBuffer;
+	vkrhi::TextureHandle mColorBuffer;
+	vkrhi::FramebufferHandle mFramebuffer;
+
+	std::shared_ptr<ShaderLoader> mShaderLoader;
+	std::shared_ptr<DescriptorTableManager> mDescriptorTableManager;
+	std::shared_ptr<BindingCache> mBindingCache;
+	std::shared_ptr<CommonRenderPasses> mHelperPass;
+};
+
+KRR_NAMESPACE_END
---
+++ src/render/rasterize/shaders/bindless.hlsl
@@ -0,0 +1,67 @@
+#pragma pack_matrix(column_major)
+
+struct ViewConstants {
+	float4x4 worldToView;
+};
+
+struct InstantConstants {
+	uint instance;
+};
+
+struct MaterialConstants {
+	Color3f baseColor;
+	float roughness;
+
+	Color3f specularColor;
+	float opacity;
+
+	Color3f emissiveColor;
+	int baseTextureIndex;
+
+	int specularTextureIndex;
+	int normalTextureIndex;
+	int emissiveTextureIndex;
+	int flags;
+};
+
+struct MeshData {
+	uint numIndices;
+	uint numVertices;
+	int indexBufferIndex;
+	uint indexOffset;
+
+	int vertexBufferIndex;
+	uint positionOffset;
+	uint prevPositionOffset;
+	uint texCoord1Offset;
+
+	uint texCoord2Offset;
+	uint normalOffset;
+	uint tangentOffset;
+	uint materialIndex;
+};
+
+ConstantBuffer<ViewConstants> g_ViewConstants : register(b0);
+[[vk::push_constant]] ConstantBuffer<InstanceConstants> g_Instance : register(b1);
+
+StructuredBuffer<MeshData> t_MeshData : register(t0);
+StructuredBuffer<MaterialConstants> t_MaterialConstants : register(t1);
+SamplerState s_MaterialSampler;
+[[vk::binding(0, 1)]] t_BindlessBuffers[] : register(t0, space1);	// register space, check it out later
+[[vk::binding(1, 1)]] t_BindlessTextures[] : register(t0, space2);
+
+void vs_main(
+	in uint i_vertexID: SV_VertexID,
+	out float4 o_position: SV_Position,
+	out float2 o_uv: TEXCOORD,
+	out uint o_material: MATERIAL) {
+	
+}
+
+void ps_main(
+	in float4 i_position: SV_Position,
+	in float2 i_uv: TEXCOORD,
+	nointerpolation in uint i_material: MATERIAL,
+	out float4 o_color: SV_Target0) {
+	
+}
