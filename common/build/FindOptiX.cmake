55ac8243fc97cb925a7a0c17c9fdc7070d2bc6f1
融雪时分 <cutedays@icloud.com>
2023-05-13 20:48:50 +0800

    [common] fix optix version > 7.5
    and confirmed that mostly compatible with cuda 12.1

--- common/build/FindOptiX.cmake
+++ common/build/FindOptiX.cmake
@@ -10,6 +10,8 @@ elseif(NOT OptiX_INSTALL_DIR)
 	find_path(searched_OptiX_INSTALL_DIR
 	  NAME include/optix.h
 	  PATHS
+	  "C:/ProgramData/NVIDIA Corporation/OptiX SDK 7.7.0"
+	  "C:/ProgramData/NVIDIA Corporation/OptiX SDK 7.6.0"
 	  "C:/ProgramData/NVIDIA Corporation/OptiX SDK 7.5.0"
 	  "C:/ProgramData/NVIDIA Corporation/OptiX SDK 7.4.0"
 	  "C:/ProgramData/NVIDIA Corporation/OptiX SDK 7.3.0"
